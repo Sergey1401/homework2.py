a390b9f3f85958937cc4f8910a04130f75b97e01
Sergey1401 <fire1401@mail.ru>
2024-07-11 16:49:45 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,15 @@
+# Практическое задание по теме "Переменные"
+# Первый вариант (как в задании)
+task_completed = 12
+stend_time  =   1.5
+course_name =  'Python'
+time_for_one_lesson =  stend_time / task_completed
+print('Курс: ',course_name,' ,всего задач: ',task_completed,' затрачено часов: ',stend_time,', среднее время выполнения: ',time_for_one_lesson,' часов.')
+
+# Практическое задание по теме "Переменные"
+# Второй ввриант(мне так больше нравится)
+task_completed = 12
+stend_time  =   1.5
+course_name =  'Python'
+time_for_one_lesson =  stend_time / task_completed
+print('\tКурс: ',course_name,'.','\nВсего задач: ',task_completed,'.','\nЗатрачено часов: ',stend_time,'.','\nСреднее время выполнения: ',time_for_one_lesson,' часов.')
